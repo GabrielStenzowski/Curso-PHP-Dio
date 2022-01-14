786bc6abc5fea4a18dee5e10638c0ec0c975e4c0
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-01-14 10:28:47 -0300

    Update index.php

--- Introdutorio1/index.php
+++ Introdutorio1/index.php
@@ -5,6 +5,30 @@
 	$categorias [] = 'Adoslecente';
 	$categorias [] = 'Adulto';
 	$categorias [] = 'Idoso';
+	//print_r($categorias);
 
-	$nome = 'Eduardo'
+	$nome = 'Gabriel';
+	$idade = 9;
+
+	//var_dump($nome);
+	//var_dump($idade);
+
+	//Metodo 1, atraves de 'ECHOS'.
+	if ($idade >= 6 && $idade <= 12) 
+	{
+		echo 'O nadador ' .$nome. ', compete na categoria Infaltil!';
+	}
+	
+	else if ($idade >= 13 && $idade <= 18)
+	{
+		echo 'O nadador ' .$nome. ', compete na categoria Adoslecente!';
+	}
+	else
+	{
+		echo 'O nadador ' .$nome. ', compete na categorias Adulto!';
+	}
+	
+
+	
+
 ?>
